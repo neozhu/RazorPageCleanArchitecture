48eb24110eb61bb9eebfba46a7830873ec2d4fcc
neozhu <neo.js.cn@gmail.com>
2021-09-11 20:32:09 +0800

    WIP: add eventhandler

--- src/Application/SalesContracts/EventHandlers/SalesContractCreatedEventHandler.cs
+++ src/Application/SalesContracts/EventHandlers/SalesContractCreatedEventHandler.cs
@@ -1,29 +1,47 @@
-﻿using System.Threading;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using CleanArchitecture.Razor.Application.Common.Interfaces;
 using CleanArchitecture.Razor.Application.Common.Models;
 using CleanArchitecture.Razor.Domain.Events;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Razor.Application.SalesContracts.EventHandlers
 {
     public class SalesContractCreatedEventHandler : INotificationHandler<DomainEventNotification<SalesContractCreatedEvent>>
     {
+        private readonly IApplicationDbContext _context;
         private readonly ILogger<SalesContractCreatedEventHandler> _logger;
 
         public SalesContractCreatedEventHandler(
+             IApplicationDbContext context,
             ILogger<SalesContractCreatedEventHandler> logger
             )
         {
+            _context = context;
             _logger = logger;
         }
-        public Task Handle(DomainEventNotification<SalesContractCreatedEvent> notification, CancellationToken cancellationToken)
+        public async Task Handle(DomainEventNotification<SalesContractCreatedEvent> notification, CancellationToken cancellationToken)
         {
             var domainEvent = notification.DomainEvent;
 
-            _logger.LogInformation("CleanArchitecture Domain Event: {DomainEvent}", domainEvent.GetType().Name);
-
-            return Task.CompletedTask;
+            var cost = await _context.PurchaseContracts.Where(x => x.ProjectId == domainEvent.Item.ProjectId)
+                     .SumAsync(x => x.ContractAmount, cancellationToken);
+            var receipt = await _context.SalesContracts.Where(x => x.ProjectId == domainEvent.Item.ProjectId)
+                    .SumAsync(x => x.ReceiptAmount, cancellationToken);
+            var constractamount = await _context.SalesContracts.Where(x => x.ProjectId == domainEvent.Item.ProjectId)
+                    .SumAsync(x => x.ContractAmount, cancellationToken);
+            var project = await _context.Projects.FirstAsync(x => x.Id == domainEvent.Item.ProjectId, cancellationToken);
+            project.ActualCost = cost;
+            project.ReceiptAmount = receipt;
+            project.ContractAmount = constractamount;
+            if (project.ActualCost > 0 && project.ContractAmount > 0)
+            {
+                project.GrossMargin = (project.ContractAmount - project.ActualCost) / project.ContractAmount;
+            }
+            await _context.SaveChangesAsync(cancellationToken);
         }
     }
 }
