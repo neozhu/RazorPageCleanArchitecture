cb6d25778563bc9bb0166e4a2dd8ee34bcd4b60a
neozhu <new163@163.com>
2021-09-13 14:53:48 +0800

    WIP: refactoring dependencyinjection.cs

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -31,7 +31,7 @@ namespace CleanArchitecture.Razor.Application
             services.AddWorkflowSteps(w => w.Name.EndsWith("Step"), Assembly.GetExecutingAssembly());
             return services;
         }
-        public static void AddWorkflowSteps(this IServiceCollection services, Func<Type, bool> predicate, params Assembly[] assemblies)
+        public static IServiceCollection AddWorkflowSteps(this IServiceCollection services, Func<Type, bool> predicate, params Assembly[] assemblies)
         {
             if (assemblies.Length == 0)
             {
@@ -45,7 +45,7 @@ namespace CleanArchitecture.Razor.Application
                .ForEach(type =>
                         services.Add(new ServiceDescriptor(type, type, ServiceLifetime.Transient))
                );
-
+            return services;
         }
     }
 }
