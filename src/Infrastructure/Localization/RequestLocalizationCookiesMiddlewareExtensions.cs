d71ad3d51b242a4148d6e09446e2642fc7f77e4a
neozhu <new163@163.com>
2021-11-11 17:32:39 +0800

    Update RequestLocalizationCookiesMiddlewareExtensions.cs

--- src/Infrastructure/Localization/RequestLocalizationCookiesMiddlewareExtensions.cs
+++ src/Infrastructure/Localization/RequestLocalizationCookiesMiddlewareExtensions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
@@ -51,5 +52,29 @@ public class RequestLocalizationCookiesMiddleware : IMiddleware
         }
 
         await next(context);
+    }
+}
+
+public class LocalizationMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var cultureKey = context.Request.Headers["Accept-Language"];
+        if (!string.IsNullOrEmpty(cultureKey))
+        {
+            if (DoesCultureExist(cultureKey))
+            {
+                var culture = new System.Globalization.CultureInfo(cultureKey);
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+            }
+        }
+
+        await next(context);
+    }
+
+    private static bool DoesCultureExist(string cultureName)
+    {
+        return CultureInfo.GetCultures(CultureTypes.AllCultures).Any(culture => string.Equals(culture.Name, cultureName, StringComparison.CurrentCultureIgnoreCase));
     }
 }
