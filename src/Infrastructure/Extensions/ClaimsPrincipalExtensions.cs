56d4e613236e6c6d0ae73113a73e2c073adaf3d0
neozhu <new163@163.com>
2021-11-19 16:28:57 +0800

    refactor code

--- src/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
+++ src/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,13 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Linq;
 using System.Security.Claims;
 using CleanArchitecture.Razor.Infrastructure.Constants.ClaimTypes;
 
-namespace SmartAdmin.WebUI.Extensions
-{
-    public static class ClaimsPrincipalExtensions
+namespace CleanArchitecture.Razor.Infrastructure.Extensions;
+
+public static class ClaimsPrincipalExtensions
     {
         public static string GetEmail(this ClaimsPrincipal claimsPrincipal)
             => claimsPrincipal.FindFirstValue(ClaimTypes.Email);
@@ -30,4 +29,4 @@ namespace SmartAdmin.WebUI.Extensions
         public static string[] GetRoles(this ClaimsPrincipal claimsPrincipal)
             => claimsPrincipal.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).ToArray();
     }
-}
+
