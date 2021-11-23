4e4bf8857e80ab7965024030790e32318d047dd4
neozhu <neo.js.cn@gmail.com>
2021-11-23 20:05:28 +0800

    update

--- src/Application/MigrationTemplateFiles/Commands/Upload/UploadMigrationTemplateFilesCommand.cs
+++ src/Application/MigrationTemplateFiles/Commands/Upload/UploadMigrationTemplateFilesCommand.cs
@@ -1,4 +1,5 @@
 
+using System.Xml.Linq;
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Packaging;
 
@@ -34,19 +35,47 @@ public class UploadMigrationTemplateFilesCommandHandler :
     }
     public async Task<Result> Handle(UploadMigrationTemplateFilesCommand request, CancellationToken cancellationToken)
     {
-         
-
         var result = await _uploadService.UploadAsync(new UploadRequest() { Data = request.Data, FileName = request.FileName, UploadType = UploadType.Document });
-
-        var doc = SpreadsheetDocument.Open(new MemoryStream(request.Data), false);
-
-            var item = new MigrationTemplateFile()
+        var item = new MigrationTemplateFile()
         {
             FilePath = result,
             Name = request.FileName,
             Description=""
         };
-        
+        var xdoc = XDocument.Load(new MemoryStream(request.Data));
+        var signature = xdoc.Descendants().Where(x => x.Name.LocalName == "Worksheet" && x.FirstAttribute.Value == "Signature").First();
+        var table = signature.Descendants().Where(x => x.Name.LocalName == "Table");
+        var index = 0;
+        foreach (var row in table.Descendants().Where(x => x.Name.LocalName == "Row" && x.Nodes().Count() == 7).Skip(1).ToList())
+        {
+            var cells = row.Nodes().ToArray();
+            var paraname = ((XElement)cells[0]).Elements().Where(x => x.Name.LocalName == "Data").First().Value;
+            var direct = ((XElement)cells[1]).Elements().Where(x=>x.Name.LocalName== "Data").First().Value;
+            if (direct == "Import Parameter")
+            {
+                index++;
+                switch (index)
+                {
+                    case 0:
+                        item.Legacy1Field = paraname;
+                        break;
+                    case 1:
+                        item.Legacy2Field = paraname;
+                        break;
+                    case 2:
+                        item.Legacy3Field = paraname;
+                        break;
+                }
+            }
+            else
+            {
+                item.NewValueField = paraname;
+            }
+            
+            
+
+        }
+
         await  _context.MigrationTemplateFiles.AddAsync(item);
         await _context.SaveChangesAsync(cancellationToken);
         return Result.Success();
