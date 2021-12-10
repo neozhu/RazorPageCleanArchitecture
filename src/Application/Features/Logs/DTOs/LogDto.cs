5b2e0a182dd280b16249526c21758d8f12a742c3
neozhu <new163@163.com>
2021-12-10 08:31:10 +0800

    update

--- src/Application/Features/Logs/DTOs/LogDto.cs
+++ src/Application/Features/Logs/DTOs/LogDto.cs
@@ -15,6 +15,9 @@ namespace CleanArchitecture.Razor.Application.Features.Logs.DTOs
         public string Exception { get; set; }
         public string UserName { get; set; }
         public string Properties { get; set; }
-    
+        public string ClientIP { get; set; }
+        public string ClientAgent { get; set; }
+        public string LogEvent { get; set; }
+
     }
 }
