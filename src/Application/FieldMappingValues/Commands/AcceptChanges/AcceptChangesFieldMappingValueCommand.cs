d4a7eb92bd5c0a87218dc4db928b336db83a14c6
neozhu <new163@163.com>
2022-01-21 17:26:02 +0800

    Update AcceptChangesFieldMappingValueCommand.cs

--- src/Application/FieldMappingValues/Commands/AcceptChanges/AcceptChangesFieldMappingValueCommand.cs
+++ src/Application/FieldMappingValues/Commands/AcceptChanges/AcceptChangesFieldMappingValueCommand.cs
@@ -26,20 +26,25 @@ public class AcceptChangesFieldMappingValuesCommandHandler : IRequestHandler<Acc
     public async Task<Result> Handle(AcceptChangesFieldMappingValuesCommand request, CancellationToken cancellationToken)
     {
         var array_id = request.Items.Select(x => x.MappingRuleId).Distinct().ToArray();
+        var isexists = false;
         foreach (var item in request.Items.DistinctBy(x => new {x.Legacy1,x.Legacy2,x.Legacy3,x.NewValue }))
         {
             switch (item.TrackingState)
             {
                 case TrackingState.Added:
                     var newitem = _mapper.Map<FieldMappingValue>(item);
-                    var isexists = await _context.FieldMappingValues.AnyAsync(x => x.MappingRuleId == newitem.MappingRuleId &&
-                                                                              x.Legacy1 == newitem.Legacy1 &&
-                                                                              x.Legacy2 == newitem.Legacy2 &&
-                                                                              x.Legacy3 == newitem.Legacy3 &&
-                                                                              x.NewValue == newitem.NewValue);
+                    isexists = await _context.FieldMappingValues.AnyAsync(x => x.MappingRuleId == newitem.MappingRuleId &&
+                                                                              (x.Legacy1 == newitem.Legacy1 || (string.IsNullOrWhiteSpace(newitem.Legacy1)  && string.IsNullOrEmpty(x.Legacy1))) &&
+                                                                              (x.Legacy2 == newitem.Legacy2 || (string.IsNullOrWhiteSpace(newitem.Legacy2)  && string.IsNullOrEmpty(x.Legacy2))) &&
+                                                                              (x.Legacy3 == newitem.Legacy3 || (string.IsNullOrWhiteSpace(newitem.Legacy3)  && string.IsNullOrEmpty(x.Legacy3)))
+                                                                              );
                     if (!isexists)
                     {
                         await _context.FieldMappingValues.AddAsync(newitem, cancellationToken);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Legacy1:{(string.IsNullOrWhiteSpace(item.Legacy1) ? "Blank":item.Legacy1)},Legacy2:{(string.IsNullOrWhiteSpace(item.Legacy2) ? "Blank" : item.Legacy2)} and Legacy3:{(string.IsNullOrWhiteSpace(item.Legacy3) ? "Blank" : item.Legacy3)} have existed.");
                     }
                     break;
                 case TrackingState.Deleted:
@@ -47,6 +52,16 @@ public class AcceptChangesFieldMappingValuesCommandHandler : IRequestHandler<Acc
                     _context.FieldMappingValues.Remove(delitem);
                     break;
                 case TrackingState.Modified:
+                    isexists = await _context.FieldMappingValues.AnyAsync(x => x.MappingRuleId == item.MappingRuleId &&
+                                                                              (x.Legacy1 == item.Legacy1 || (string.IsNullOrWhiteSpace(item.Legacy1) && string.IsNullOrEmpty(x.Legacy1))) &&
+                                                                              (x.Legacy2 == item.Legacy2 || (string.IsNullOrWhiteSpace(item.Legacy2) && string.IsNullOrEmpty(x.Legacy2))) &&
+                                                                              (x.Legacy3 == item.Legacy3 || (string.IsNullOrWhiteSpace(item.Legacy3) && string.IsNullOrEmpty(x.Legacy3)))
+                                                                              );
+                    if (isexists)
+                    {
+
+                        throw new InvalidOperationException($"Legacy1:{(string.IsNullOrWhiteSpace(item.Legacy1) ? "Blank" : item.Legacy1)},Legacy2:{(string.IsNullOrWhiteSpace(item.Legacy2) ? "Blank" : item.Legacy2)} and Legacy3:{(string.IsNullOrWhiteSpace(item.Legacy3) ? "Blank" : item.Legacy3)} have existed.");
+                    }
                     var edititem = await _context.FieldMappingValues.FindAsync(new object[] { item.Id }, cancellationToken);
                     edititem = _mapper.Map(item, edititem);
                     _context.FieldMappingValues.Update(edititem);
